b8e67b722e576f1908462c49e7a1dd2b92c97ecc
Jorge Morales <jmorales113@gmail.com>
2020-03-05 11:09:28 -0800

    Wrote to ReadMe and working.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# kdkd
-            
+
+# fkdkfd
+
+[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
+
+## Description
+
+fjkdfjd
+
+## Table of Contents
+
+* [Installation](#Installation)
+
+* [Usage](#Usage)
+
+* [License](#License)
+
+* [Contributing](#Contributing)
+
+* [Tests](#Tests)
+
+* [Questions](#Questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+    fjkdfjkd
+
+## Usage
+
+fjdkfjkd
+
+## License
+
+fjkdfjkd
+
+## Contributing
+
+fjkdfjkd
+
+## Tests
+
+To perform a test, run the following command:
+
+    fjkdfjdk
+
+## Questions
+
+<img src="https://avatars2.githubusercontent.com/u/57970306?s=460&v=4"
+alt="avatar" style="border-radius: 16px" width="30" />
+
+If you have any questions about the repo, open an issue or contact at dkkd
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
 const axios = require("axios")
-// const generateMarkdown = require("./utils/generateMarkdown")
-// const api = require("./utils/api")
 
 inquirer.prompt([
     {
@@ -53,36 +51,66 @@ inquirer.prompt([
     .then (function(response){
         const api = `https://api.github.com/users/${response.username}`
         axios
-        .get(api).then(function(res){
+        .get(api).then(function(){
         
     const data = `
-    # ${response.title}
-        `
-        fs.writeFile("test.md", data, function(){
-            console.log("Wrote to test.md")
+# ${response.title}
+
+[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
+
+## Description
+
+${response.description}
+
+## Table of Contents
+
+* [Installation](#Installation)
+
+* [Usage](#Usage)
+
+* [License](#License)
+
+* [Contributing](#Contributing)
+
+* [Tests](#Tests)
+
+* [Questions](#Questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+    ${response.installation}
+
+## Usage
+
+${response.usage}
+
+## License
+
+${response.license}
+
+## Contributing
+
+${response.contributing}
+
+## Tests
+
+To perform a test, run the following command:
+
+    ${response.tests}
+
+## Questions
+
+<img src="https://avatars2.githubusercontent.com/u/57970306?s=460&v=4"
+alt="avatar" style="border-radius: 16px" width="30" />
+
+If you have any questions about the repo, open an issue or contact at ${response.username}`
+
+        fs.writeFile("ReadMe.md", data, function(){
+            console.log("Wrote to ReadMe.md")
         })
         })
 
     })
 
-//     .then (function(data) {
-//      const username = data.username
-//         api.getUser(username).then(function(getUserResponse){
-//             const markDown = generateMarkdown(data, getUserResponse)
-//             return fs.writeToFile("ReadMe.md", markDown)
-//         })
-//     })
-    
-//     .then(function(){
-//         console.log("Wrote to ReadMe.md")
-//     })
-//     .catch(function(err){
-//         console.log(err)
-//     })
-    
-
-// // function init() {
-
-// // }
-
-// // init();
